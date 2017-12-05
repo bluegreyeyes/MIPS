01800aa21fd3eac7777233755064f45fed9c55f3
Athuro83 <30749120+Athuro83@users.noreply.github.com>
2017-12-05 22:51:23 +0100

    Mise en place du main #1
    Rédaction de la structure principale de la fonction main :
    - Déclarations des variables
    - Réalisation de la boucle de lecture principale
    
    A faire :
    - Construction de la trame binaire en fonction de l'instruction décodée

--- rendu_1-traducteur_hexa/main.c
+++ rendu_1-traducteur_hexa/main.c
@@ -16,14 +16,69 @@
  * =================================================
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "trad.h"
+#include "file.h"
 
 int main(int argc, char* argv[]){
 	 
 	/* Variables */
+	int bin_trame[TRAME_BIN_LEN] = {0},
+	    fill_trame[TRAME_BIN_LEN] = {0},
+	    reg = 0,
+	    imm_value = 0;
+
+	char hexa_trame[TRAME_HEXA_LEN] = "",
+	     instr[INSTR_MAX_LEN] = "";
+
+	FILE *input_file = NULL,
+	     *hexa_file = NULL;
 
 	/* Initialisation */
+	initTrame(fill_trame);
+
+	/* Check if enough arguments are provided */
+	if(argc < 3){
+		printf("Please enter the name of the file to convert and the name of the hexa file to be created.\n");
+		exit(-1);
+	}
+
+	/* TODO : Fonction de sécurisation du nom du fichier */
+
+	/* Opening the needed files to proceed */
+	input_file = openFile(argv[1], "r");
+	hexa_file = openFile(argv[2], "w");
 
 	/* Code */
+
+	do
+	{
+
+		readInstr(input_file, instr);
+
+		/* We have reached the end of the file, exiting the loop */
+		if(instr[0] == '\0')
+			break;
+
+		initTrame(bin_trame);
+
+		switch(ifInstr(instr)){
+
+		}
+
+		/* Translating binary trame to hexadecimal trame */
+		translateBinTrameToHexa(bin_trame, hexa_trame);
+
+		/* Writing hexa trame in the new file */
+		writeInFile(hexa_file, hexa_trame);
+
+	}while(1);
+
+	/* Safely closing the files before the end of the program */
+	closeFile(argv[1], input_file);
+	closeFile(argv[2], hexa_file);
+
 	return(0);
 }
